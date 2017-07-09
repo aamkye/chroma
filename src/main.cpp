97145d1319b2ea8a72825d1269b347170dfea088
Amadeusz Kryze <amadeusz.kryze@gmail.com>
2017-07-09 20:17:58 +0200

    Edidet sample

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
   Chroma::Painter c;
-  uint32_t num = 2;
+  uint32_t num = 107;
 
   std::cout << c.text("Text", {Chroma::Fore::iGreen}) << std::endl;
   std::cout << c.text(num + 5, {Chroma::Fore::iRed}) << std::endl;
